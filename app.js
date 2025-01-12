95b71c573db447b0a72688611cb03cf042e14af1
codinci <dkibau@gmail.com>
2025-01-12 07:02:32 +0300

    dockeried application

--- app.js
+++ app.js
@@ -11,7 +11,6 @@ const app = express();
 app.use(express.json());
 app.use("/api/cryptos", router);
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(configure.MONGODB_URI, {
 	serverApi: {
 		version: ServerApiVersion.v1,
@@ -22,15 +21,12 @@ const client = new MongoClient(configure.MONGODB_URI, {
 
 async function run() {
 	try {
-		// Connect the client to the server (optional starting in v4.7)
 		await client.connect();
-		// Send a ping to confirm a successful connection
 		await client.db("admin").command({ ping: 1 });
 		logger.info(
-		"Pinged your deployment. You successfully connected to MongoDB!"
+			"Pinged your deployment. You successfully connected to MongoDB!"
 		);
 	} finally {
-		// Ensures that the client will close when you finish/error
 		await client.close();
 	}
 }
